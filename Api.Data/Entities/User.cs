86e55ee4ed7e5daa3d71da0195168af2a999ad2e
Felipe Sousa <sousa.felipe@outlook.com>
2025-11-11 11:40:21 -0300

    feat: mapeamento da entidade User com NHibernate XML

--- Api.Data/Entities/User.cs
+++ Api.Data/Entities/User.cs
@@ -1,19 +1,16 @@
 namespace Api.Data.Entities;
 
 
-public sealed class User
+public class User
 {
-    public Guid Id { get; init; }
-    public string Name { get; set; } = string.Empty;
-    public string Surname { get; set; } = string.Empty;
-    public string Email { get; set; } = string.Empty;
-    public string Password { get; set; } = string.Empty;
-    public string PhoneNumber { get; set; } = string.Empty;
-    public DateOnly BirthDate { get; set; }
+    public virtual long Id { get; protected set; }
+    public virtual string Name { get; set; } = string.Empty;
+    public virtual string Surname { get; set; } = string.Empty;
+    public virtual string Email { get; set; } = string.Empty;
+    public virtual string Password { get; set; } = string.Empty;
+    public virtual string PhoneNumber { get; set; } = string.Empty;
+    public virtual DateOnly BirthDate { get; set; }
 
 
-    public User(Guid? id = null)
-    {
-        Id = (id == null) ? Guid.NewGuid() : id.Value;
-    }
+    public User() { }
 }
