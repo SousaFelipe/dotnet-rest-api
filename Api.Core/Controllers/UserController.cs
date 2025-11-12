dbafb94c3f0193849a7b8581af341cbee451d75c
Felipe Sousa <sousa.felipe@outlook.com>
2025-11-11 21:19:55 -0300

    feat: implementação da rota GET - /users/{id} com documentação dos tipos de retorno com Swagger

--- Api.Core/Controllers/UserController.cs
+++ Api.Core/Controllers/UserController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using Api.Core.Dtos;
+using Api.Data.Entities;
 using Api.Service.Interfaces;
 
 
@@ -12,28 +14,56 @@ public class UserController(IUserService userService) : ControllerBase
     private IUserService UserService { get; init; } = userService;
 
 
-    [HttpGet]
-    public async Task<ActionResult> Get()
+    [HttpGet("{id}")]
+    [ProducesResponseType(typeof(UserResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status500InternalServerError)]
+    public async Task<ActionResult> Get(long id)
+    {
+        var user = await UserService.ReadUser(id);
+        return Ok(user);
+    }
+
+
+    [HttpGet("{page}/{size}")]
+    [ProducesResponseType(typeof(PagedResult<UserResponse>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status500InternalServerError)]
+    public async Task<ActionResult> GetPaged(int page, int size)
     {
         return Ok();
     }
 
 
     [HttpPost]
-    public async Task<ActionResult> Post()
+    [ProducesResponseType(typeof(UserResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status500InternalServerError)]
+    public async Task<ActionResult<User>> Post()
     {
         return Ok();
     }
 
 
     [HttpPut]
-    public async Task<ActionResult> Put()
+    [ProducesResponseType(typeof(UserResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status500InternalServerError)]
+    public async Task<ActionResult<User>> Put()
     {
         return Ok();
     }
 
 
     [HttpDelete]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status500InternalServerError)]
     public async Task<ActionResult> Delete()
     {
         return Ok();
