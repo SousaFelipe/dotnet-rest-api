60b3e46c7d5c5dd2bd2b915029294baf9d05380b
Felipe Sousa <sousa.felipe@outlook.com>
2025-11-12 19:08:09 -0300

    fix: exibindo a quantidade de ítens por página, considerando se o total de ítens é menor do que a quantidade requerida

--- Api.Service/Dtos/PagedResponse.cs
+++ Api.Service/Dtos/PagedResponse.cs
@@ -6,5 +6,5 @@ public class PagedResponse<T>(List<T> items, int total, int page, int size)
     public List<T> Items { get; set; } = items;
     public int Total { get; set; } = total;
     public int Page { get; set; } = page;
-    public int Size { get; set; } = size;
+    public int Size { get; set; } = (size > total) ? total : size;
 }
